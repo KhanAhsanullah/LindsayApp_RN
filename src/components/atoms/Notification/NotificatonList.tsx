703da41801aed85132475822efdeb9d96a22eb23
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2024-12-03 14:32:17 +0500

    notifications listing

--- src/components/atoms/Notification/NotificatonList.tsx
+++ src/components/atoms/Notification/NotificatonList.tsx
@@ -1,53 +1,71 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList, TouchableOpacity, Image } from "react-native";
 import { Text, View } from "react-native-ui-lib";
 import { Typography } from "../Typography";
 import { IMAGES, theme } from "../../../constants";
 import { commonStyles } from "../../../globalStyle";
+import { useDispatch } from "react-redux";
+import { MainActions } from "../../../redux/actions/MainActions";
 
 const NotificatonList = (props: any) => {
   const { onPress } = props;
+  const [listing, setListing] = useState([]);
+  const dispatch = useDispatch();
+  const getNotificationsListing = async () => {
+    try {
+      await dispatch(MainActions.GetNotificationsListing()).then((v) => {
+        let status = v.meta.requestStatus;
+        if (status == "fulfilled") {
+          setListing(v?.payload);
+        }
+      });
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getNotificationsListing();
+  }, []);
+
   const DATA = [
     {
       id: 1,
       title: "Congratulations",
       image: IMAGES.no1,
-      time:'03:30 PM',
+      time: "03:30 PM",
       subTitle: "Your house cleaning service is successfully completed.",
     },
     {
       id: 2,
       title: "Schedule",
       image: IMAGES.no2,
-      time:'11:30 PM',
+      time: "11:30 PM",
       subTitle: "Your painting service is scheduled on 25th March.",
     },
     {
       id: 3,
       title: "Payment Success",
       image: IMAGES.no3,
-      time:'03:40 PM',
+      time: "03:40 PM",
       subTitle: "Your payment process is completed.",
     },
     {
       id: 4,
       title: "Order Placed",
       image: IMAGES.no4,
-      time:'01:00 PM',
+      time: "01:00 PM",
       subTitle: "Your house cleaning service is placed successfully.",
     },
     {
       id: 4,
       title: "Monthly Offer",
       image: IMAGES.no5,
-      time:'03:20 PM',
+      time: "03:20 PM",
       subTitle: "30% off on plumbing service untill 27th March.",
     },
     {
       id: 4,
       title: "Order Canceled",
       image: IMAGES.no6,
-      time:'11:30 PM',
+      time: "11:30 PM",
       subTitle: "Your repairing order is canceled.",
     },
   ];
@@ -57,25 +75,32 @@ const NotificatonList = (props: any) => {
         <TouchableOpacity onPress={onPress}>
           <View row marginV-20>
             <Image
-              source={item.image}
-              style={{ width: 65, height: 65 }}
+              source={IMAGES.no1}
+              style={{ width: 65, height: 40 }}
               resizeMode="contain"
             />
             <View marginL-20 flex>
               <View row spread>
-              <Typography  size={theme.fontSize.medium} textType={"semiBold"}>
-                {item.title}
-              </Typography>
-              <Typography color={theme.color.descColor} size={theme.fontSize.medium}>
-                {item.time}
-              </Typography>
+                <Typography
+                  style={{ textTransform: "capitalize" }}
+                  size={theme.fontSize.medium}
+                  textType={"semiBold"}
+                >
+                  {item.title}
+                </Typography>
+                <Typography
+                  color={theme.color.descColor}
+                  size={theme.fontSize.medium}
+                >
+                  {item.created_date}
+                </Typography>
               </View>
               <Typography
-                style={{ width: "70%" }}
+                style={{ width: "70%", textTransform: "capitalize" }}
                 color={theme.color.descColor}
                 size={theme.fontSize.extraSmall12}
               >
-                {item.subTitle}
+                {item.message}
               </Typography>
             </View>
           </View>
@@ -87,7 +112,7 @@ const NotificatonList = (props: any) => {
 
   return (
     <FlatList
-      data={DATA}
+      data={listing}
       renderItem={_renderItem}
       keyExtractor={(item: any) => item.id.toString()}
       showsVerticalScrollIndicator={false}
