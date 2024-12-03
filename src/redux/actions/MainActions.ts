1f7d5ce38d8b401331b653bf4590a3f590b00d4a
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2024-12-03 14:32:37 +0500

    actions

--- src/redux/actions/MainActions.ts
+++ src/redux/actions/MainActions.ts
@@ -1,78 +1,77 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import client from '../../utils/AxiosInterceptor';
-import { convertDataToFormData } from '../../utils/Constants';
-import { endpoints } from '../../utils/Endpoints';
-import { setLoading } from '../slices/OtherSlice';
-import { AuthActions } from './AuthActions';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import client from "../../utils/AxiosInterceptor";
+import { convertDataToFormData } from "../../utils/Constants";
+import { endpoints } from "../../utils/Endpoints";
+import { setLoading } from "../slices/OtherSlice";
+import { AuthActions } from "./AuthActions";
 
 export const MainActions = {
   GetCategoryData: createAsyncThunk(
-    'main/category',
+    "main/category",
     async ({ id }: { id: string }, thunkApi) => {
-      let apiCall = await client.get(
-        endpoints.Category(id),
-      );
+      let apiCall = await client.get(endpoints.Category(id));
       return apiCall.data?.response?.data;
-    },
+    }
   ),
   FilterCategoryDataByTitle: createAsyncThunk(
-    'main/category_filter_title',
+    "main/category_filter_title",
     async ({ title }: { title: string }, thunkApi) => {
-      let apiCall = await client.get(
-        endpoints.FilterCategoryByTitle(title),
-      );
+      let apiCall = await client.get(endpoints.FilterCategoryByTitle(title));
       return apiCall.data?.response?.data[0];
-    },
+    }
   ),
   GetAllCategoryData: createAsyncThunk(
-    'main/all_category',
+    "main/all_category",
     async (data, thunkApi) => {
-      let apiCall = await client.get(
-        endpoints.AllCategory,
-      );
+      let apiCall = await client.get(endpoints.AllCategory);
       return apiCall.data?.response?.data;
-    },
+    }
   ),
   GetAllExcerciseCategory: createAsyncThunk(
-    'main/GetAllExcerciseCategory',
+    "main/GetAllExcerciseCategory",
     async (data, thunkApi) => {
-      let apiCall = await client.get(
-        endpoints.ExcerciseCategories,
-      );
+      let apiCall = await client.get(endpoints.ExcerciseCategories);
       return apiCall.data?.response?.data;
-    },
+    }
   ),
   GetAllExcercises: createAsyncThunk(
-    'main/GetAllExcercises',
+    "main/GetAllExcercises",
     async (data, thunkApi) => {
-      let apiCall = await client.get(
-        endpoints.ExcerciseCategories,
-      );
+      let apiCall = await client.get(endpoints.ExcerciseCategories);
       return apiCall.data?.response?.data;
-    },
+    }
   ),
   GetAllAvailableDates: createAsyncThunk(
-    'main/GetAllAvailableDates',
+    "main/GetAllAvailableDates",
     async (data, thunkApi) => {
-      let apiCall = await client.get(
-        endpoints.Bookings(data?.date),
-      );
+      let apiCall = await client.get(endpoints.Bookings(data?.date));
       return apiCall.data?.response?.data;
-    },
+    }
   ),
-  BookSlot: createAsyncThunk(
-    'main/BookSlot',
+  GetNotificationsListing: createAsyncThunk(
+    "main/GetNotificationsListing",
     async (data, thunkApi) => {
-      thunkApi.dispatch(setLoading(true))
-      let apiCall = await client.post(
-        endpoints.BookSlot,
-        {
-          booking_id: data?.booking_id
-        }
-      );
-      thunkApi.dispatch(setLoading(false))
+      thunkApi.dispatch(setLoading(true));
+      let apiCall = await client.get(endpoints.NotificationsListing);
+      thunkApi.dispatch(setLoading(false));
       return apiCall.data?.response?.data;
-    },
+    }
   ),
-
+  BookSlot: createAsyncThunk("main/BookSlot", async (data, thunkApi) => {
+    thunkApi.dispatch(setLoading(true));
+    let apiCall = await client.post(endpoints.BookSlot, {
+      booking_id: data?.booking_id,
+    });
+    thunkApi.dispatch(setLoading(false));
+    return apiCall.data?.response?.data;
+  }),
+  DeleteAccount: createAsyncThunk(
+    "main/DeleteAccount",
+    async (data, thunkApi) => {
+      thunkApi.dispatch(setLoading(true));
+      let apiCall = await client.post(endpoints.DeleteAccount);
+      thunkApi.dispatch(setLoading(false));
+      return apiCall.data?.response;
+    }
+  ),
 };
